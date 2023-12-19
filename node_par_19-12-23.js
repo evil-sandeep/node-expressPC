f37ebd135a3d259841bf9a947523340978e6c1d1
sandeep kumar sahoo <chandansandeep2000@gmail.com>
2023-12-19 12:56:31 +0530

    single API endpoint that returns a list of books.

--- node_par_19-12-23.js
+++ node_par_19-12-23.js
@@ -48,42 +48,72 @@
 
 // ----------------------------------------------------------------------------------------------------
 // Question: Middleware and Error Handling
-const express = require('express');
-const bodyParser = require('body-parser');
+// const express = require('express');
+// const bodyParser = require('body-parser');
 
-const app = express();
-const port = 3000;
+// const app = express();
+// const port = 3000;
 
-app.use((req, res, next) => {
-    console.log(`[${new Date().toISOString()} ${req.method}Request recevied ${req.path}Present]`)
-    next()
-});
+// app.use((req, res, next) => {
+//     console.log(`[${new Date().toISOString()} ${req.method}Request recevied ${req.path}Present]`)
+//     next()
+// });
 
-app.use((req, res, next) => {
-    const authorizationHeder = req.get('Authorization')
+// app.use((req, res, next) => {
+//     const authorizationHeder = req.get('Authorization')
 
-    if (authorizationHeder) {
-        console.log(`Authorization present ${authorizationHeder}`)
-    } else {
-        console.log('authorization is not present')
-    }
-    next();
-})
+//     if (authorizationHeder) {
+//         console.log(`Authorization present ${authorizationHeder}`)
+//     } else {
+//         console.log('authorization is not present')
+//     }
+//     next();
+// })
 
-app.get('/api/error', (req, res) => {
-    try {
-        throw new Error('Internal Server Error')
-    } catch (error) {
-        next(error);
-    }
-})
+// app.get('/api/error', (req, res) => {
+//     try {
+//         throw new Error('Internal Server Error')
+//     } catch (error) {
+//         next(error);
+//     }
+// })
 
-app.use((err, req, res, next) => {
-    console.error(`Error : ${err.message}`)
-    res.status(500).json({ error: 'Internal server error' })
-})
+// app.use((err, req, res, next) => {
+//     console.error(`Error : ${err.message}`)
+//     res.status(500).json({ error: 'Internal server error' })
+// })
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
+// app.listen(port, () => {
+//     console.log(`Server is running on port ${port}`);
+// });
 
+//----------------------------------------------------------------------------------------------------------
+
+// Create a simple Express.js server with a single API endpoint that returns a list of books. Assume the list of books is an array of objects, each containing a title and an author. The endpoint should be accessible at http://localhost:3001/books.
+
+// const express = require('express')
+// const app = express()
+// const port = 3001;
+
+// const books = [
+//     { id: 1, title: 'book1', author: 'author1' },
+//     { id: 2, title: 'book2', author: 'author2' }
+// ]
+
+// app.get('/books', (req, res) => {
+//     res.json(books)
+// })
+
+// app.get('/books/:id', (req, res) => {
+//     const booksId = parseInt(req.params.id)
+//     const book = books.find(b => b.id === booksId)
+//     if (!book) {
+//         res.status(400).send('Books not find in api')
+//         return
+//     }
+//     res.json(book)
+// })
+
+// app.listen(port, () => {
+//     console.log(`Server is running on http://localhost:${port}`);
+// });
